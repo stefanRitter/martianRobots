c15d165073185c2189ba406d63983962f305b41a
stefanRitter <stefan@stefanritter.com>
2013-05-18 10:30:07 +0100

    move sass out of /public

--- config.rb
+++ config.rb
@@ -2,8 +2,8 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
+css_dir = "public/stylesheets"
+sass_dir = "sass/"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
@@ -16,9 +16,4 @@ javascripts_dir = "javascripts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :sass
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,6 +10,7 @@
 // *********************************************************************************************************** APP
 var App = new (Backbone.View.extend({
 
+  // class definitions
   Models: {},
   Views: {},
   Collections: {},
@@ -18,49 +19,102 @@ var App = new (Backbone.View.extend({
 
   events: {},
 
+  // global models
+  mars: {},
+  robot: {},
+
   start: function() {
     Backbone.history.start({ pushState: true });
-  },
-
-  mars: {},
-  robot: {}
+  }
 }))({el: document.body});
 
 
 // *********************************************************************************************************** MODELS
-App.Models.Mars = Backbone.Model.extend({});
+App.Models.Mars = Backbone.Model.extend({
+  // TODO: register mars' grid
+});
 App.mars = new App.Models.Mars();
 
 
 App.Models.Robot = Backbone.Model.extend({
-
   degrees:     ['0deg', '90deg',  '180deg',  '270deg'],
-  orientation: ['N',    'E',      'S',       'W']
+  orientation: ['N',    'E',      'S',       'W'],
+  currOrient: 0,
+
+  getOrientation: function() {
+    return this.degrees[this.currOrient];
+  }
 });
 App.robot = new App.Models.Robot();
 
 
+App.Models.DeadRobots = Backbone.Collection.extend({
+  model: App.Models.Robot
+  // TODO: collect dead robots
+});
+
+
 // *********************************************************************************************************** VIEWS
-App.Views.Mars = Backbone.View.extend({});
+App.Views.Mars = Backbone.View.extend({
+  className: 'mars',
+
+  render: function() {
+    // TODO: render mars, robot, and dead robots
+    return this;
+  }
+});
 
 
-App.Views.Input = Backbone.View.extend({});
+App.Views.Input = Backbone.View.extend({
+  className: 'input',
+
+  template: _.template( '<form class="newProjectForm">' +
+                          '<input type="text" name="commands" value="enter commands here" />' +
+                          '<input type="submit" />' +
+                        '</form>'),
+
+  events: { submit: "readInput"},
+
+  render: function() {
+    this.$el.html( this.template( this.model.attributes ));
+    return this;
+  },
+
+  readInput: function() {
+    // TODO: handle input
+  }
+});
 
 
-App.Views.Output = Backbone.View.extend({});
+App.Views.Output = Backbone.View.extend({
+  className: 'output',
+
+  initialize: function() {
+    // TODO: listen to robot changes
+  },
+
+  render: function() {
+    // TODO: display robot position on change
+    return this;
+  }
+});
 
 
 // *********************************************************************************************************** ROUTER
 App.router = new (Backbone.Router.extend({
-
   routes: {
     "(/)*path": "index"
   },
 
   index: function() {
-    var tempView1 = new App.Views.Mars(),
-        tempView2 = new App.Views.Input(),
-        tempView3 = new App.Views.Output();
+    var mars = new App.Views.Mars({ model: App.mars }),
+        input = new App.Views.Input({ model: App.robot }),
+        output = new App.Views.Output({ model: App.robot }),
+        $app = $('#app');
+
+    $app.append(mars.render().el);
+    $app.append(input.render().el);
+    $app.append(output.render().el);
   }
 }))();
 
